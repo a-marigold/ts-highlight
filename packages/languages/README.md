b03b9df9525741cebaf951562ab517aaf69e7d0c
NextKolya <phelixgluy@gmail.com>
2026-01-31 22:55:53 +0600

    add turborepo

--- packages/languages/README.md
+++ packages/languages/README.md
@@ -1,1 +1,11 @@
 # <p align='center'> **@ts-highlight/languages** </p>
+
+### Overview
+
+This package includes `languageConfig`\`s for most of languages
+
+### Supported languages
+
+- rust
+- go
+- zig
---
+++ packages/languages/rollup.config.mjs
@@ -0,0 +1,34 @@
+import { defineConfig } from 'rollup';
+
+import typescript from '@rollup/plugin-typescript';
+import dts from 'rollup-plugin-dts';
+
+import terser from '@rollup/plugin-terser';
+
+export default defineConfig([
+    {
+        input: './src/index.ts',
+
+        external: ['./src/__tests__/**'],
+
+        output: {
+            file: './dist/index.js',
+            format: 'esm',
+        },
+        plugins: [typescript({ exclude: ['**__tests__/**'] }), terser()],
+    },
+
+    {
+        input: './src/index.ts',
+
+        external: ['./src/__tests__/**'],
+
+        output: {
+            file: './dist/index.d.ts',
+
+            format: 'esm',
+        },
+
+        plugins: [dts()],
+    },
+]);
